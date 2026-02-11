6c250d499c56ccdb3484e8ab7d495d377dbba82e
Leandro Batista <leandrobouwierphp@gmail.com>
2026-02-10 22:26:17 -0300

    repositorio crud

--- tesHospital.js
+++ tesHospital.js
@@ -1,16 +1,10 @@
 import { Address } from './src/shared/Address.js';
 import { EmergencyContact } from './src/shared/EmergencyContact.js';
 
-import { Allergy } from './src/patient/Allergy.js';
-import { Appointment } from './src/patient/Appointment.js';
-import { Exam } from './src/patient/Exam.js';
 import { Patient } from './src/patient/Patient.js';
 
-import { Diagnosis } from './src/patient/record/Diagnosis.js';
-import { Treatment } from './src/patient/record/Treatment.js';
-import { Medication } from "./src/patient/record/Medication.js";
+import { PatientRepository } from "./src/patient/PatientRepository.js";
 
-import { Doctor } from './src/doctor/Doctor.js';
 
 const address = new Address(
   'Rua das Flores',
@@ -24,7 +18,7 @@ const emergencyContact = new EmergencyContact('Maria Silva', '(11) 88888-88888')
 
 
 const patient = new Patient(
-  '1',
+  1,
   '123-456-789-00',
   'João Silva',
   '1990-01-01',
@@ -36,43 +30,9 @@ const patient = new Patient(
   emergencyContact,
 );
 
+const patientRepository = new PatientRepository();
 
+patientRepository.add(patient.id, patient);
 
-const doctor = new Doctor(
-  '1',
-  'CRM/SP 123456',
-  'Dr. Carlos',
-  ['Cardiologista', 'Clínica Geral'],
-  '(11) 77777-7777',
-);
-doctor.addWorkingHours('Segunda', '14:00-18:00');
-
-const appointment = new Appointment(
-  '1',
-  '2025-10-10',
-  patient,
-  doctor,
-  'Dor no peito',
-  'Agendada',
-  'Paciente relatou dor no peito após esforço físico.',
-);
-
-const exam = new Exam(
-  '1',
-  'Hemograma',
-  'Normal',
-  '2025-10-11',
-  'Laboratório X',
-  'Dr. Ana',
-  patient,
-);
-
-patient.addAllergy(new Allergy('Penicilina'));
-patient.scheduleAppointment(appointment);
-patient.addExam(exam);
-
-patient.medicalRecord.addDiagnosis(new Diagnosis('Hipertensão'));
-patient.medicalRecord.addTreatment(new Treatment('Redução no consumo de sal'));
-patient.medicalRecord.addMedication(new Medication("Captopril", "25mg"));
-
-console.log(exam);
+const foundPatient = patientRepository.findById(1);
+console.log(foundPatient);
